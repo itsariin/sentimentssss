d7d57ffa51c705a5726363a134d4d026fa893dc9
Arin Jain <arinjain349@gmail.com>
2023-09-26 21:07:50 +0530

    FINISH

---
+++ lets_see.py
@@ -0,0 +1,13 @@
+# import GetOldTweets3 as hope
+#
+#
+# def get_tweets():
+#     tweetCriteria = hope.manager.TweetCriteria().setQuerySearch('corona') \
+#         .setSince("2019-05-01") \
+#         .setUntil("2021-09-30") \
+#         .setMaxTweets(10)
+#     tweets = hope.manager.TweetManager.getTweets(tweetCriteria)
+#     print(tweets)
+#
+#
+# get_tweets()
